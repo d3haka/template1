74d3f9561cd8fc8c5e509245211960f7717ecc66
d3haka <arian.rassaee@gmail.com>
2025-12-28 12:48:37 +0330

    feat: added prettier & tailwindcss & relavant plugins.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { twMerge } from "tw-merge";
 import "./styles.css";
 
 function App() {
   return (
     <>
-      <h1>hellooo</h1>
+      <h1 className={twMerge("font-bold text-red-500")}>hellooo</h1>
     </>
   );
 }
